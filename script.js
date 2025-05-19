34d73dc5270b5acf5687704f8b73852d38651ae4
Aluno <brenodelimadelima.c.cagnin@gmail.com>
2025-05-19 16:37:24 -0300

    rgb_MUDACOR

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('keydown', function(event) {
+    switch (event.key.toUpperCase()) {
+        case 'R':
+            document.body.style.backgroundColor = 'red';
+            break;
+        case 'G':
+            document.body.style.backgroundColor = 'green';
+            break;
+        case 'B':
+            document.body.style.backgroundColor = 'blue';
+            break;
+        case 'W':
+            document.body.style.backgroundColor = 'white';
+            break;
+    }
+});
